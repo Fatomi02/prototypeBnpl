7af282b8617ca31671210eadc2bb7929d1801488
Fatomi02 <fatomiabdulrahmon@gmail.com>
2025-05-25 02:06:37 -0700

    feat: vercel json refs: none

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bnpl-service-2-06lc.onrender.com/",
+  baseURL: "https://bnpl-service-try-7ewi.onrender.com/",
   // withCredentials: true,
 });
 
